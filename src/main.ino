11fa4c5d1db48ee9c3bc79342ca8ce69d0cf95d3
Parham Alvani <parham.alvani@gmail.com>
2017-05-24 00:24:08 +0430

    Mqtt connection initiation was added.

--- src/main.ino
+++ src/main.ino
@@ -1,5 +1,6 @@
 #include <SPI.h>
 #include <Ethernet.h>
+#include <PubSubClient.h>
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
@@ -11,7 +12,11 @@ IPAddress ip(192, 168, 73, 9);
 // Initialize the Ethernet server library
 // with the IP address and port you want to use
 // (port 80 is default for HTTP):
-EthernetServer server(80);
+EthernetClient ethClient;
+PubSubClient client;
+
+// Agent identification
+String uid;
 
 void setup() {
 	// Open serial communications and wait for port to open:
@@ -21,62 +26,18 @@ void setup() {
 	}
 
 
-	// start the Ethernet connection and the server:
+	// start the Ethernet connection and the mqtt client
 	Ethernet.begin(mac, ip);
-	server.begin();
-	Serial.print("server is at ");
-	Serial.println(Ethernet.localIP());
+	client.setClient(ethClient);
+	client.setServer("192.168.73.6", 1883);
+	client.connect("I1820-agent");
+
+	// generate agent identification
+	uid = "RoomanArduino1";
 }
 
 
 void loop() {
-	// listen for incoming clients
-	EthernetClient client = server.available();
-	if (client) {
-		Serial.println("new client");
-		// an http request ends with a blank line
-		boolean currentLineIsBlank = true;
-		while (client.connected()) {
-			if (client.available()) {
-				char c = client.read();
-				Serial.write(c);
-				// if you've gotten to the end of the line (received a newline
-				// character) and the line is blank, the http request has ended,
-				// so you can send a reply
-				if (c == '\n' && currentLineIsBlank) {
-					// send a standard http response header
-					client.println("HTTP/1.1 200 OK");
-					client.println("Content-Type: text/html");
-					client.println("Connection: close");  // the connection will be closed after completion of the response
-					client.println("Refresh: 5");  // refresh the page automatically every 5 sec
-					client.println();
-					client.println("<!DOCTYPE HTML>");
-					client.println("<html>");
-					// output the value of each analog input pin
-					for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-						int sensorReading = analogRead(analogChannel);
-						client.print("analog input ");
-						client.print(analogChannel);
-						client.print(" is ");
-						client.print(sensorReading);
-						client.println("<br />");
-					}
-					client.println("</html>");
-					break;
-				}
-				if (c == '\n') {
-					// you're starting a new line
-					currentLineIsBlank = true;
-				} else if (c != '\r') {
-					// you've gotten a character on the current line
-					currentLineIsBlank = false;
-				}
-			}
-		}
-		// give the web browser time to receive the data
-		delay(1);
-		// close the connection:
-		client.stop();
-		Serial.println("client disconnected");
-	}
+	client.publish("I1820/parham-home/agent/ping", "{}");
+	delay(10);
 }
