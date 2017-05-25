e9825e3e8f3b4072d3f98d55c583d1c12c5c998f
Parham Alvani <parham.alvani@gmail.com>
2017-05-25 11:34:39 +0430

    Let's build our Rooman without any mqtt :P

--- src/main.ino
+++ src/main.ino
@@ -1,6 +1,7 @@
 #include <SPI.h>
 #include <Ethernet.h>
-#include <PubSubClient.h>
+#include <WebServer.h>
+
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
@@ -12,32 +13,51 @@ IPAddress ip(192, 168, 73, 9);
 // Initialize the Ethernet server library
 // with the IP address and port you want to use
 // (port 80 is default for HTTP):
-EthernetClient ethClient;
-PubSubClient client;
+EthernetServer server(80);
 
-// Agent identification
-String uid;
+// This creates an instance of the webserver.  By specifying a prefix
+// of "", all pages will be at the root of the server.
+#define PREFIX ""
+WebServer webserver(PREFIX, 80);
+
+
+void helloCmd(WebServer &server, WebServer::ConnectionType type, char *, bool)
+{
+	// this line sends the standard "we're all OK" headers back to the browser
+	server.httpSuccess();
+
+	// if we're handling a GET or POST, we can output our data here.
+	// For a HEAD request, we just stop after outputting headers.
+	if (type != WebServer::HEAD)
+	{
+		/* this defines some HTML text in read-only memory aka PROGMEM.
+		 * This is needed to avoid having the string copied to our limited
+		 * amount of RAM. */
+		P(helloMsg) = "Hello form arduino";
+
+		/* this is a special form of print that outputs from PROGMEM */
+		server.printP(helloMsg);
+	}
+}
+
 
 void setup() {
-	// Open serial communications and wait for port to open:
-	Serial.begin(9600);
-	while (!Serial) {
-		; // wait for serial port to connect. Needed for native USB port only
-	}
-
-
-	// start the Ethernet connection and the mqtt client
+	// Initialize the Ethernet adapter
 	Ethernet.begin(mac, ip);
-	client.setClient(ethClient);
-	client.setServer("192.168.73.6", 1883);
-	client.connect("I1820-agent");
 
-	// generate agent identification
-	uid = "RoomanArduino1";
+	// /hello -> helloCmd
+	webserver.addCommand("hello", &helloCmd);
+
+	// start the webserver
+	webserver.begin();
+
 }
 
 
 void loop() {
-	client.publish("I1820/parham-home/agent/ping", "{}");
-	delay(10);
+	char buff[64];
+	int len = 64;
+
+	// process incoming connections one at a time forever
+	webserver.processConnection(buff, &len);
 }
