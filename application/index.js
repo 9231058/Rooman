918a58417fcbf788f8be501c3ad8d54e07b58352
Parham Alvani <parham.alvani@gmail.com>
2017-06-08 00:06:14 +0430

    Provides rest api for rooman.

--- application/index.js
+++ application/index.js
@@ -8,15 +8,36 @@
  * +===============================================
  */
 const RTPSession = require('krtp').RTPSession;
+const express = require('express');
 
 const s = new RTPSession(1373);
+const app = express();
+
+let t = 0;
+let h = 0;
 
 s.on('message', (msg) => {
-  ht = msg.payload.toString();
+  let ht = msg.payload.toString();
   t = parseFloat(ht.split("\n")[0]);
   h = parseFloat(ht.split("\n")[1]);
   console.log(t);
   console.log(h);
   console.log(msg.timestamp);
   console.log(msg.sequenceNumber);
+});
+
+app.get('/t', function (req, res) {
+  res.json({
+    temperature: t
+  });
+});
+
+app.get('/h', function (req, res) {
+  res.json({
+    humidity: h
+  });
+});
+
+app.listen(1373, function () {
+    console.log('* http on 0.0.0.0:1373')
 });
