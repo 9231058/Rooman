16ff14263f7884fbebd6506dfc8165536541b14c
Parham Alvani <parham.alvani@gmail.com>
2017-06-17 03:59:43 +0430

    Navigation bar was added.

--- application/src/App.jsx
+++ application/src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
 class App extends React.Component {
+ constructor(props) {
+   super(props);
+   this.state = {location: 'Home'};
+ }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+        <button className="navbar-toggler navbar-toggler-right" type="button"
+          data-toggle="collapse" data-target="#navbar" aria-controls="navbar"
+          aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <a className="navbar-brand" href="/">Rooman</a>
+
+        <div className="collapse navbar-collapse" id="navbar">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#"
+                onClick={this.setState({location: 'Home'})}>
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
