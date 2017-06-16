4a58a04885d995ee9059219171d5083f4adfbba6
Parham Alvani <parham.alvani@gmail.com>
2017-06-16 22:51:01 +0430

    Basic source of react.js was added.

---
+++ application/src/index.jsx
@@ -0,0 +1,7 @@
+// Application entrypoint.
+
+// Render the top-level React component
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('react-root'));
