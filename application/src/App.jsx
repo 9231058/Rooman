d4608cc6d6a43da863e79bb762c1114d4e4fc3c4
Parham Alvani <parham.alvani@gmail.com>
2017-06-17 04:46:09 +0430

    App component was improved and corrected.

--- application/src/App.jsx
+++ application/src/App.jsx
@@ -1,32 +1,46 @@
 import React from 'react';
+import Temperature from './Temperature.jsx';
 
 class App extends React.Component {
- constructor(props) {
-   super(props);
-   this.state = {location: 'Home'};
- }
+  constructor(props) {
+    super(props);
+    this.state = {location: 'Home'};
+  }
+
+  changeLocation(newLocation) {
+    this.setState({
+      location: newLocation
+    });
+  }
 
   render() {
     return (
-      <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-        <button className="navbar-toggler navbar-toggler-right" type="button"
-          data-toggle="collapse" data-target="#navbar" aria-controls="navbar"
-          aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <a className="navbar-brand" href="/">Rooman</a>
+      <div>
+        <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+          <button className="navbar-toggler navbar-toggler-right" type="button"
+            data-toggle="collapse" data-target="#navbar" aria-controls="navbar"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <a className="navbar-brand" href="/">Rooman</a>
 
-        <div className="collapse navbar-collapse" id="navbar">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#"
-                onClick={this.setState({location: 'Home'})}>
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </li>
-          </ul>
+          <div className="collapse navbar-collapse" id="navbar">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#"
+                  onClick={() => {
+                    this.changeLocation('Home');
+                  }}>
+                  Home <span className="sr-only">(current)</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div>
+          <Temperature />
         </div>
-      </nav>
+      </div>
     );
   }
 }
