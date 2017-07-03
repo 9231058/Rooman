7d302e73e466207d42789623c1d30d6d4d78ed67
Parham Alvani <parham.alvani@gmail.com>
2017-07-03 22:58:41 +0430

    Removes index.js because we do not need it anymore.

--- application/src/components/index.jsx
+++
@@ -1,9 +0,0 @@
-// Application entrypoint.
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-ReactDOM.render(<App />, document.getElementById('react-root'));
