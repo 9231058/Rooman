c5e1e52e01572cbfd3dc778333e762be70331f3c
Parham Alvani <parham.alvani@gmail.com>
2017-07-03 22:58:30 +0430

    App component was improved.

--- application/src/components/App.jsx
+++ application/src/components/App.jsx
@@ -1,37 +1,39 @@
-import React from 'react';
-import Temperature from './Temperature.jsx';
+import React from 'react'
+import Temperature from './Temperature.jsx'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {location: 'Home'};
+  constructor (props) {
+    super(props)
+    this.state = {location: 'Home'}
   }
 
-  changeLocation(newLocation) {
+  changeLocation (newLocation) {
     this.setState({
       location: newLocation
-    });
+    })
   }
 
-  render() {
+  render () {
     return (
       <div>
-        <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-          <button className="navbar-toggler navbar-toggler-right" type="button"
-            data-toggle="collapse" data-target="#navbar" aria-controls="navbar"
-            aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+        <nav className='navbar navbar-toggleable-md navbar-light bg-faded'>
+          <button className='navbar-toggler navbar-toggler-right' type='button'
+            data-toggle='collapse' data-target='#navbar' aria-controls='navbar'
+            aria-expanded='false' aria-label='Toggle navigation'>
+            <span className='navbar-toggler-icon' />
           </button>
-          <a className="navbar-brand" href="/">Rooman</a>
+          <a className='navbar-brand' href='/'>Rooman</a>
 
-          <div className="collapse navbar-collapse" id="navbar">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="#"
+          <div className='collapse navbar-collapse' id='navbar'>
+            <ul className='navbar-nav mr-auto'>
+              <li className='nav-item active'>
+                <a className='nav-link' href='#'
                   onClick={() => {
-                    this.changeLocation('Home');
+                    this.changeLocation('Home')
                   }}>
-                  Home <span className="sr-only">(current)</span>
+                  Home <span className='sr-only'>(current)</span>
                 </a>
               </li>
             </ul>
@@ -41,8 +43,8 @@ class App extends React.Component {
           <Temperature />
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
