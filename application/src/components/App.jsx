6759a7d9df8a6b7ca3f9c4a3d39eedcf7f3261e8
Parham Alvani <parham.alvani@gmail.com>
2017-07-03 23:29:57 +0430

    App component was improved.

--- application/src/components/App.jsx
+++ application/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Temperature from './Temperature.jsx'
+import Header from './Header.jsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -18,29 +19,11 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        <nav className='navbar navbar-toggleable-md navbar-light bg-faded'>
-          <button className='navbar-toggler navbar-toggler-right' type='button'
-            data-toggle='collapse' data-target='#navbar' aria-controls='navbar'
-            aria-expanded='false' aria-label='Toggle navigation'>
-            <span className='navbar-toggler-icon' />
-          </button>
-          <a className='navbar-brand' href='/'>Rooman</a>
-
-          <div className='collapse navbar-collapse' id='navbar'>
-            <ul className='navbar-nav mr-auto'>
-              <li className='nav-item active'>
-                <a className='nav-link' href='#'
-                  onClick={() => {
-                    this.changeLocation('Home')
-                  }}>
-                  Home <span className='sr-only'>(current)</span>
-                </a>
-              </li>
-            </ul>
+        <Header />
+        <div className='container'>
+          <div className='col-6'>
+            <Temperature />
           </div>
-        </nav>
-        <div>
-          <Temperature />
         </div>
       </div>
     )
